a63b8ec2e6435c2b878ddb86cbbe6d575e4cc901
owenw-28 <113171443+owenw-28@users.noreply.github.com>
2022-09-12 17:59:50 +0100

    Add files via upload

---
+++ Brampton Manor Academy/Challenge 1/Challenge1.py
@@ -0,0 +1,33 @@
+def metres(rods):
+    metres = rods*5.0292
+    return metres
+
+def feet(rods):
+    feet = metres(rods)/(1/0.3048)
+    return feet
+
+def miles(rods):
+    miles = metres(rods)/1609.34
+    return miles
+
+def furlong(rods):
+    furlong = rods/40
+    return furlong
+
+def time(rods):
+    time = miles(rods)/3.1*60
+    return time
+
+def print_info(rods):
+    print("You input",rods,"rods.") 
+    print("Conversions")
+    print("Meters: ",metres(rods))
+    print("Miles: ",miles(rods))
+    print("Feet: ",feet(rods))
+    print("Furlongs: ",furlong(rods))
+    print("Minutes to walk ",rods,"rods: ",time(rods))
+
+def main():
+    rods = float(input("Input rods: "))
+    print_info(rods)
+main()
---
+++ Brampton Manor Academy/Challenge 1/Testing1.py
@@ -0,0 +1,12 @@
+import unittest
+from Challenge1 import *
+
+class TestSum(unittest.TestCase):
+
+    def test_metres(self):
+        self.assertEqual(metres(self)), 50.292
+
+if __name__ == '__main__':
+    unittest.main()
+
+# I am confused about how to do this

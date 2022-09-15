25e77cb25fb6e26ba130ee51aff6d6d22760fb0c
owenw-28 <113171443+owenw-28@users.noreply.github.com>
2022-09-15 16:43:21 +0100

    Update Testing1.py

--- Brampton Manor Academy/Challenge 1/Testing1.py
+++ Brampton Manor Academy/Challenge 1/Testing1.py
@@ -4,9 +4,8 @@ from Challenge1 import *
 class TestSum(unittest.TestCase):
 
     def test_metres(self):
-        self.assertEqual(metres(self)), 50.292
+        self.assertEqual(metres(10), 50.292)
 
 if __name__ == '__main__':
     unittest.main()
 
-# I am confused about how to do this

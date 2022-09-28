23512528782f1e4c7aafbdf0097f38bfdc21f13c
owenw-28 <113171443+owenw-28@users.noreply.github.com>
2022-09-28 14:49:01 +0100

    Update Challenge10.py

--- Brampton Manor Academy/Challenge10.py
+++ Brampton Manor Academy/Challenge10.py
@@ -22,11 +22,23 @@ def process_results(rows):
         goals_home = row[3]
         goals_away = row[4]
         winner = row[5]
+        ref = row[6]
+        home_shots = row[7]
+        away_shots = row[8]
+        home_shots_target = row[9]
+        away_shots_target = row[10]
+        home_foul = row[11]
+        away_foul = row[12]
+        home_yellow = row[15]
+        away_yellow = row[16]
+        home_red = row[17]
+        away_red = row[18]
+        
         if home not in dictionary:
-            dictionary[home] = [0,0,0,0,0]
+            dictionary[home] = [0,0,0,0,0,0,0] #win,draw,loss,gd,points,accuracy,fouls per game
 
         if away not in dictionary:
-            dictionary[away] = [0,0,0,0,0]
+            dictionary[away] = [0,0,0,0,0,0,0]
 
         if winner == "D":
             dictionary[home][1] +=1
@@ -45,20 +57,16 @@ def process_results(rows):
             dictionary[home][2] +=1
             dictionary[home][3] += int(goals_home) - int(goals_away)
             dictionary[away][3] += int(goals_away) - int(goals_home)
+        dictionary[home][5] += (int(home_shots_target)/int(home_shots))/38
+        dictionary[away][5] += (int(away_shots_target)/int(away_shots))/38
     return(dictionary)
+     
             
 if __name__ == "__main__":
     file_contents = read_csv(csv_file)
     table = (process_results(file_contents))
     for key, value in sorted(table.items(), key=lambda e: e[1][4], reverse=True):
-    if __name__ == "__main__":
-    file_contents = read_csv(csv_file)
-    table = (process_results(file_contents))
-    for key, value in sorted(table.items(), key=lambda e: e[1][4], reverse=True):
-    i = 0
-    for i in range (0,19):
-        print(f"{key[i], {value[4]},{value[0]},{value[1]},{value[2]},{value[3]}")
-        print(key, value)
+        print(key,value)
 
 
 

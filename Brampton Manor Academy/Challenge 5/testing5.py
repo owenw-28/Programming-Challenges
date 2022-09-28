e426f6a457a7301ccc4a4a11d61b501dcc911740
owenw-28 <113171443+owenw-28@users.noreply.github.com>
2022-09-28 19:40:18 +0100

    Add files via upload

---
+++ Brampton Manor Academy/Challenge 5/testing5.py
@@ -0,0 +1,16 @@
+#Does not work
+
+import unittest
+from Challenge5 import *
+
+class TestSum(unittest):
+
+    def TestCalculate1(self):
+        self.assertNotTrue(calculate(666666, 1999998)) 
+
+    def TestCalculate2(self):
+        self.assertTrue(calculate(142857, 428571))
+
+if __name__ == '__main__':
+    unittest.main()
+

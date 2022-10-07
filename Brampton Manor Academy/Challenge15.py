12fdc7c2d2900b32cb4ceb4c6d9c1e3377a254e9
owenw-28 <113171443+owenw-28@users.noreply.github.com>
2022-10-07 09:34:17 +0100

    Add files via upload

---
+++ Brampton Manor Academy/Challenge15.py
@@ -0,0 +1,38 @@
+import csv
+from pathlib import Path
+
+import urllib.request
+import html
+
+csv_file = Path("south.csv")
+
+def read_csv(csv_file):
+     csv_contents = []
+     with open(csv_file) as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")                   
+        for row in reader:
+            csv_contents.append(row)
+     return csv_contents
+    ## read the specified csv file just like Challenge 10
+    
+def read_html(html_file):
+    with open(html_file) as htmlfile:
+        html = htmlfile.read()
+    return html
+    ## read a html file as a regular file
+
+def process(csv, html):
+    position = html.find('background-image: url("link1");')
+    ## replace link1...link5 in html with corresponding values from csv
+    ## Similarly do for initials1...intitials5 and name1...name5
+  
+def write_html(path, html):
+    ## write the new contents into an html file
+
+
+if __name__ == "__main__":
+    csv = read_csv("south.csv")
+    html = read_html("south.html")
+    html = process(csv, html)
+    write_html("south_final.html", html)
+

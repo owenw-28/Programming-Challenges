dc869256284c53a2849747d848125b35edabaf3e
owenw-28 <113171443+owenw-28@users.noreply.github.com>
2022-10-07 09:45:36 +0100

    Add files via upload

--- Brampton Manor Academy/Challenge15.py
+++ Brampton Manor Academy/Challenge15.py
@@ -22,14 +22,22 @@ def read_html(html_file):
     ## read a html file as a regular file
 
 def process(csv, html):
+    i = 0
     position = html.find('background-image: url("link1");')
+    position = position.replace("link1",csv[i,0])
+    while position !=-1 and count<=5:
+        start = html.find('background-image: url("')
+        stop = html.find('");',start)
+        html[start:stop] = csv[i,0]
+        i += 1
+        
     ## replace link1...link5 in html with corresponding values from csv
     ## Similarly do for initials1...intitials5 and name1...name5
-  
+    
 def write_html(path, html):
     ## write the new contents into an html file
 
-
+ 
 if __name__ == "__main__":
     csv = read_csv("south.csv")
     html = read_html("south.html")

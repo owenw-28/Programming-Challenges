c0bcb8d076b6ac0cf08cf7cd984515466a04364c
owenw-28 <113171443+owenw-28@users.noreply.github.com>
2022-10-13 18:39:44 +0100

    Update Try&Except.py

--- Brampton Manor Academy/Try&Except.py
+++ Brampton Manor Academy/Try&Except.py
@@ -12,11 +12,15 @@ def main():
     num = int(input("Please enter a natural number: "))
     if symbol not in "+-*/" or num <0:
         raise Exception
-    
-    matrix = [[0 for x in range(1)] for y in range(num)]
-    for i in range(0,num+1):
-        print(matrix)
-        #for j in range(0,num+1):
+
+    rows, cols = (num+1,num+1)
+    i = 0
+    matrix = [[0 for i in range(rows)]for j in range(cols)]
+    for row in matrix:
+        matrix[i][0] = i
+        i+=1
+        print(row)
+        
         
     
 main()

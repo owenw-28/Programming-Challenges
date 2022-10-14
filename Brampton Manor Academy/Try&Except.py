24578e95e13eb6ed6650a6ce7c44ed5a4e2a4fd5
owenw-28 <113171443+owenw-28@users.noreply.github.com>
2022-10-14 09:30:23 +0100

    Update Try&Except.py

--- Brampton Manor Academy/Try&Except.py
+++ Brampton Manor Academy/Try&Except.py
@@ -8,24 +8,39 @@ ops = {
     }
 
 def main():
-    symbol = input("Please enter an operator: +, -, * or /: ")
-    num = int(input("Please enter a natural number: "))
-    if symbol not in "+-*/" or num <0:
-        raise Exception
+    try:
+        symbol = input("Please enter an operator: +, -, * or /: ")
+        num = int(input("Please enter a natural number: "))
+        if symbol not in "+-*/":
+            raise TypeError
+        elif num <0:
+            raise ValueError
+    except TypeError:
+        print("You did not input a valid operation")
+    except ValueError:
+        print("Please do not enter a number below 1")
+    
+        
 
     rows, cols = (num+1,num+1)
     matrix = [[0 for i in range(rows)]for j in range(cols)] #Initialise matrix
+    firstln = [x for x in range(num+1)]
+    firstln = [str(i) for i in firstln]
+    print(f"{symbol} {' '.rjust(1,' ').join(firstln)}")
+    print("----------------")
     for i in range(0,num+1):
         for j in range(0,num+1):
             try:
                 value = ops[symbol](i,j)
                 matrix[i][j] = value
             except ZeroDivisionError:
-                value = 0
+                value = "-"
                 matrix[i][j] = value
             finally:
-                print(round(matrix[i][j],1),end=" ")
-                
+                try:
+                    print(round(matrix[i][j],1),end=" ")
+                except:
+                    print(matrix[i][j],end=" ")
         print("\n")
     
 main()

3bbd6d9469cdee1a4d21b839f8389e97f3270514
owenw-28 <113171443+owenw-28@users.noreply.github.com>
2022-10-23 17:48:28 +0100

    Update testing5.py

--- Brampton Manor Academy/Challenge 5/testing5.py
+++ Brampton Manor Academy/Challenge 5/testing5.py
@@ -1,14 +1,14 @@
 import unittest
 from Challenge5 import *
 
-class TestSum(unittest):
-
-    def TestCalculate1(self):
-        self.assertNotTrue(calculate(666666, 1999998)) 
+class TestSum(unittest.TestCase):
 
     def TestCalculate2(self):
-        self.assertTrue(calculate(142857, 428571))
+        self.assertNotEqual(calculate(666666, 1999998), 1999998)
 
+    def TestCalculate2(self):
+        self.assertEqual(calulate(142587, 427761), 427761)
+        
 if __name__ == '__main__':
     unittest.main()
 

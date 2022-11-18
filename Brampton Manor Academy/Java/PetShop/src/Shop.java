d9db3cfc788b947bd4b51bdde74ef3f14510bdc4
owenw-28 <113171443+owenw-28@users.noreply.github.com>
2022-11-18 14:43:54 +0000

    Update Shop.java

--- Brampton Manor Academy/Java/PetShop/src/Shop.java
+++ Brampton Manor Academy/Java/PetShop/src/Shop.java
@@ -1,40 +1,44 @@
 public class Shop {
     public static void main(String[] args) {
         Dog trevor = new Dog("Trevor", "Corgi", 12);
-        String name = trevor.getName();
-        String breed = trevor.getBreed();
-        int age = trevor.getAge();
-        System.out.println(name+" "+breed+" "+age);
+        String name1 = trevor.getName();
+        String breed1 = trevor.getBreed();
+        int age1 = trevor.getAge();
+        System.out.println(name1+" "+breed1+" "+age1);
         trevor.bark();
         trevor.eatBiscuit();
         trevor.getOlder();
+        System.out.println("");
 
         Dog steve = new Dog("Steve", "Shih Tzu", 5);
-        String name = steve.getName();
-        String breed = steve.getBreed();
-        int age = steve.getAge();
-        System.out.println(name+" "+breed+" "+age);
-        trevor.bark();
-        trevor.eatBiscuit();
-        trevor.getOlder();
+        String name2 = steve.getName();
+        String breed2 = steve.getBreed();
+        int age2 = steve.getAge();
+        System.out.println(name2+" "+breed2+" "+age2);
+        steve.bark();
+        steve.eatBiscuit();
+        steve.getOlder();
+        System.out.println("");
 
         Dog scooby = new Dog("Scooby", "Great Dane", 7);
-        String name = scooby.getName();
-        String breed = scooby.getBreed();
-        int age = scooby.getAge();
-        System.out.println(name+" "+breed+" "+age);
-        trevor.bark();
-        trevor.eatBiscuit();
-        trevor.getOlder();
+        String name3 = scooby.getName();
+        String breed3 = scooby.getBreed();
+        int age3 = scooby.getAge();
+        System.out.println(name3+" "+breed3+" "+age3);
+        scooby.bark();
+        scooby.eatBiscuit();
+        scooby.getOlder();
+        System.out.println("");
 
         Dog brian = new Dog("Brian", "Labrador", 10);
-        String name = scooby.getName();
-        String breed = scooby.getBreed();
-        int age = scooby.getAge();
-        System.out.println(name+" "+breed+" "+age);
-        trevor.bark();
-        trevor.eatBiscuit();
-        trevor.getOlder();
+        String name4 = brian.getName();
+        String breed4 = brian.getBreed();
+        int age4 = brian.getAge();
+        System.out.println(name4+" "+breed4+" "+age4);
+        brian.bark();
+        brian.eatBiscuit();
+        brian.getOlder();
+        System.out.println("");
     }
 }
 
